3566cebc0a50b72a1438f879e857bb20c4e56290
Gabriel Reis <bielreiscavm@gmail.com>
2025-08-20 11:18:57 -0300

    init: Iniciando o estudo Python

---
+++ par_impar.py
@@ -0,0 +1,7 @@
+# Declarar uma variavel que ira guardar um numero
+valor=12
+if(valor % 2 == 0):
+    print("Este valor é par")
+else:
+    print("Este valor é impar")
+    
---
+++ ponto_virgula.py
@@ -0,0 +1,3 @@
+# Declaração de variaveis com conteudo e separação utilizado ponto e virgula
+pais="Brazil"; uf="SP"; cidade="suzano"; espaco=' - '
+print(pais+espaco+uf+espaco+cidade)
